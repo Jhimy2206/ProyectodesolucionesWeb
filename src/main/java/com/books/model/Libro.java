52800bc3c59ab05232e7fcff966fc9734d6a6ee5
Jhimy2206 <jimibanda123456789@gmail.com>
2025-10-28 19:10:32 -0500

    Actualización de modelos y controlador de usuario y libro

---
+++ src/main/java/com/books/model/Libro.java
@@ -0,0 +1,14 @@
+package com.books.model;
+
+public class Libro {
+
+    private Long idLibro;
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private String categoria;
+    private int anioPublicacion;
+    private int cantidadTotal;
+    private int cantidadDisponible;
+
+}
